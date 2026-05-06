61ac3c5324bbd59d8b0ec3333534282f59d96294
Chablau03 <90479774+Chablau03@users.noreply.github.com>
2026-05-05 22:44:54 -0300

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,31 @@
+// lista.js
+const listaDeItens = [
+    {
+        id: "mago_peito_01",
+        nome: "Peitoral do Eclipse Arcano",
+        tipo: "equipamento",
+        slot: "peito",
+        img: "https://i.imgur.com/xeugGBJ.png"
+    },
+    {
+        id: "mago_luvas_01",
+        nome: "Luvas do Eclipse Arcano",
+        tipo: "equipamento",
+        slot: "luvas",
+        img: "https://i.imgur.com/FfUQjK6.png"
+    },
+    {
+        id: "mago_calca_01",
+        nome: "Calça do Eclipse Arcano",
+        tipo: "equipamento",
+        slot: "pernas",
+        img: "https://i.imgur.com/LkBprw7.png"
+    },
+    {
+        id: "mago_gorro_01",
+        nome: "Capuz do Eclipse Arcano",
+        tipo: "equipamento",
+        slot: "gorro",
+        img: "https://i.imgur.com/H7WZPUM.png"
+    }
+];
